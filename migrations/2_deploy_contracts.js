fae45511ed7e1665fb0891c8b1693eb1506ecf57
Jose Carvajal <josecarcar1@gmail.com>
2021-06-28 16:44:05 -0500

    Emptying tests to redo them myself

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,23 +1,25 @@
-const DappToken = artifacts.require('DappToken')
-const DaiToken = artifacts.require('DaiToken')
-const TokenFarm = artifacts.require('TokenFarm')
+const DappToken = artifacts.require("DappToken");
+const DaiToken = artifacts.require("DaiToken");
+const TokenFarm = artifacts.require("TokenFarm");
 
+// migrate blockchain - add new smart contracts to the blockchain
+// accounts = the ones shown on ganache
 module.exports = async function(deployer, network, accounts) {
-  // Deploy Mock DAI Token
-  await deployer.deploy(DaiToken)
-  const daiToken = await DaiToken.deployed()
+  // deploy mock DAI Token
+  await deployer.deploy(DaiToken);
+  const daiToken = await DaiToken.deployed();
 
-  // Deploy Dapp Token
-  await deployer.deploy(DappToken)
-  const dappToken = await DappToken.deployed()
+  // deploy dapp token
+  await deployer.deploy(DappToken);
+  const dappToken = await DappToken.deployed();
 
-  // Deploy TokenFarm
-  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address)
-  const tokenFarm = await TokenFarm.deployed()
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address);
+  const tokenFarm = await TokenFarm.deployed();
 
-  // Transfer all tokens to TokenFarm (1 million)
-  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
+  // transfer dapp tokens to token farm; these will be used as the reward from the farm
+  // when someone stakes a dai token
+  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000'); // 1 million with 18 decimals 0's
 
-  // Transfer 100 Mock DAI tokens to investor
-  await daiToken.transfer(accounts[1], '100000000000000000000')
+  // transfer 100 dai tokens to an investor (one of the accounts on ganache)
+  await daiToken.transfer(accounts[1], '100000000000000000000');
 }
