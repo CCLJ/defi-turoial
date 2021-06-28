b12dd71606f3180aafaa5ae7fc32c27a84e6e26f
Jose Carvajal <josecarcar1@gmail.com>
2021-06-28 17:44:44 -0500

    Stake tokens as investor

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -8,9 +8,32 @@ contract TokenFarm {
     DappToken public dappToken;
     DaiToken public daiToken;
 
+    address[] public stakers;
+    mapping(address => uint) public stakingBalance; // a hash essentially
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
     // runs once when a smart contract is deployed to the blockchain
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
+    }
+
+    // 1. Stake tokens (Deposit)
+    function stakeTokens(uint _amount) public {
+        // transfer dai tokens from investors wallet to this smart contract
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+
+        // update balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        // add user to stakers array if it's first time staking
+        if(!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+        // update staking status
+        hasStaked[msg.sender] = true;
+        isStaking[msg.sender] = true;
     }
 }
--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -13,9 +13,9 @@ require('chai')
   .use(require('chai-as-promised'))
   .should()
 
-  // owner: person who deployed dai token to network
 contract('TokenFarm', ([owner, investor]) => {
   // write tests here, trying to replicate migrations
+  // owner: person who deployed dai token to network
   let daiToken, dappToken, tokenFarm
 
   before(async () => {
@@ -54,6 +54,30 @@ contract('TokenFarm', ([owner, investor]) => {
     it('contract has tokens transferred', async () => {
       let balance = await dappToken.balanceOf(tokenFarm.address)
       assert.equal(balance, tokens('1000000'))
+    })
+  })
+
+  describe('Farming tokens', async () => {
+    it('rewards investors for staking mDai tokens', async () => {
+      let result
+      result = await daiToken.balanceOf(investor)
+      assert.equal(result.toString(), tokens('100'), 'investor mDai balance correct before staking')
+
+      // approving is done in test only, and in frontend app
+      await daiToken.approve(tokenFarm.address, tokens('100'), { from: investor })
+      await tokenFarm.stakeTokens(tokens('100'), { from: investor })
+
+      result = await daiToken.balanceOf(investor)
+      assert.equal(result.toString(), tokens('0'), 'investor mDai balance correct after staking')
+
+      result = await daiToken.balanceOf(tokenFarm.address)
+      assert.equal(result.toString(), tokens('100'), 'Token Farm mDai balance correct after staking')
+
+      result = await tokenFarm.stakingBalance(investor)
+      assert.equal(result.toString(), tokens('100'), 'investor staking balance correct after staking')
+
+      result = await tokenFarm.isStaking(investor)
+      assert.equal(result, true, 'investor staking status correct after staking')
     })
   })
 })
